e7718eef71bb629945a475e1737a7dd8a4118a10
James Birmingham <james@dimsum.tv>
2015-10-19 08:26:42 +0100

    install requires ...

--- setup.py
+++ setup.py
@@ -6,9 +6,13 @@ from setuptools import setup
 setup(name='apiary',
       version='0.0.1',
       description='Python version of the Ruby Apiary client',
-      author='James Birmingham',
+      author='James Birmingham, Apiary',
       author_email='james@dimsum.tv',
       packages=['apiary', 'apiary.command'],
+      install_requires=[
+          'click==5.1',
+          'Jinja2==2.8'
+      ],
       entry_points={
           "console_scripts": [
               "apiary = apiary.__main__:main"
