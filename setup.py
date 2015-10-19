990b0c57260b4dce7d5a85a9f388120f826d44e5
James Birmingham <james@dimsum.tv>
2015-10-19 12:45:20 +0100

    setup.py fiddling

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 
 
 setup(name='apiary',
-      version='0.0.4',
+      version='0.0.5',
       description='Python version of the Ruby Apiary client',
       author='James Birmingham, Apiary',
       author_email='james@dimsum.tv',
-      packages=['apiary', 'apiary.command', 'apiary.helpers'],
+      packages=['apiary', 'apiary.command', 'apiary.helpers', 'apiary.file_templates'],
       install_requires=[
           'click',
           'Jinja2==2.8'
@@ -19,4 +19,4 @@ setup(name='apiary',
           ]
       },
       url="https://github.com/Jimflip/apiary-client.git",
-      download_url="https://github.com/Jimflip/apiary-client/archive/0.0.4.tar.gz")
+      download_url="https://github.com/Jimflip/apiary-client/archive/0.0.5.tar.gz")
